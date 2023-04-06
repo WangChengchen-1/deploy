7738fa63381405967beadd3d940927d638099f0f
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-06 15:32:48 +0800

    feat(is ok): is ok
    is ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 /* 
     Vue Router 不再是一个类，而是一组函数。现在你不用再写 new Router()，而是要调用 createRouter:
 */
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 
 const router = createRouter({
-  history: createWebHistory(),//history mode
+  // history: createWebHistory(),//history mode
+  history: createWebHashHistory(),//history mode
   routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
+  base:'./',
   // 配置别名
   resolve: {
     alias: [
