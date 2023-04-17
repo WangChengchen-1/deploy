85054ebe25bcdb2c8434259d77f997bdc627f41e
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-17 10:09:44 +0800

    feat(增加节流函数替换防抖函数): 增加节流函数替换防抖函数
    增加节流函数替换防抖函数

--- src/utils/resize.js
+++ src/utils/resize.js
@@ -1,4 +1,4 @@
-import { debounce } from "@/utils/utils.js";
+import { throttle } from "@/utils/utils.js";
 import pinia from "@/stores/index.js";
 import { useAppStore } from "@/stores/modules/app.js";
 const { toggleSideBarHide } = useAppStore(pinia);
@@ -7,8 +7,10 @@ function resize() {
   let innerWidth = window.innerWidth; //获取文档显示区的宽度
   if (innerWidth <= 1000) {
     toggleSideBarHide(true);
+  } else {
+    toggleSideBarHide(false);
   }
 }
 resize();
-const debounceFn = debounce(resize, 300);
-window.addEventListener("resize", debounceFn);
+const throttleFn = throttle(resize, 300);
+window.addEventListener("resize", throttleFn);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,6 +8,35 @@ export function debounce(fn, delay) {
     if (timer) {
       clearTimeout(timer);
     }
-    timer = setTimeout(fn, delay);
+    timer = setTimeout(() => {
+      typeof fn === "function" && fn();
+    }, delay);
+  };
+}
+
+/**
+ * 节流函数（定时器版本）
+ * 概念：每隔 n 秒触发一次
+ */
+export function throttle(fn, delay = 500) {
+  let timer = null;
+  return () => {
+    if (!timer) {
+      timer = setTimeout(() => {
+        typeof fn === "function" && fn.apply(this, arguments);
+        timer = null;
+      }, delay);
+    }
+  };
+}
+function throttleTime(fn, delay = 500) {
+  let timer = null,
+    startTime = 0;
+  return () => {
+    let now = +new Date(); // 转换为时间戳
+    if (now - startTime > delay) {
+      typeof fn === "function" && fn.apply(this, arguments);
+      startTime = now;
+    }
   };
 }
