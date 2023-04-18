d026448f8d4d7b37fba8bcfe4d30cfe150a0de57
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-18 15:30:57 +0800

    fix(修复class='right'元素的宽度问题): 修复class='right'元素的宽度问题
    修复class='right'元素的宽度问题

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,7 +5,7 @@
   <div class="main-content">
     <Sidebar class="left" />
     <!-- 二级路由出口 -->
-    <div class="right">
+    <div class="right" :style="{ width: rightWidth }">
       <Navbar />
       <el-scrollbar height="calc(100vh - 84px)">
         <router-view></router-view>
@@ -17,14 +17,20 @@
 <script  setup>
 import Sidebar from "./components/Sidebar/index.vue";
 import Navbar from "./components/Navbar/index.vue";
+import { useAppStore } from '@/stores/modules/app.js'
+
+const isCollapse = computed(() => useAppStore().isCollapse);
+const rightWidth = ref('');
+
+watchEffect(() => {
+  rightWidth.value = isCollapse.value ? 'calc(100% - 64px)' : 'calc(100% - 200px)';
+})
+
 
 </script>
 
 <style lang="scss" scoped>
 .main-content {
   display: flex;
-  .right {
-    width: 100%;
-  }
 }
 </style>
