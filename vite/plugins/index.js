4829b8cc0d9de5e5580b79205f9865dd167b9cdf
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-18 16:31:06 +0800

    fix(插件路径引入错误): 插件路径引入错误
    插件路径引入错误

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -1,10 +1,10 @@
 import vue from "@vitejs/plugin-vue";
 
 import autoImport from "./auto-import.js";
-import svgSprites from "./rollup-plugin-svg-sprites";
+import svgSprites from "./svg-sprites.js";
 
 export default function createVitePlugins() {
-  const vitePlugins = [vue(), autoImport(),svgSprites()];
+  const vitePlugins = [vue(), autoImport(), svgSprites()];
 
   return vitePlugins;
 }
