ce389f65bc40623fbab1a09b482589537004d701
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-21 14:58:57 +0800

    fix(修复tags 关闭后的跳转问题): 修复tags 关闭后的跳转问题
    修复tags 关闭后的跳转问题

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -5,117 +5,121 @@
  * @name: tags-view
 -->
 <template>
-    <div class="tags-view-container">
-        <ScrollPane>
-            <router-link class="tags-view-item" :class="{ active: isActive(tag) }" v-for="(tag, index) in visitedViews "
-                :to="{ path: tag.path, query: tag.query, fullPath: tag.fullPath }" :key="tag.path">
-                <span class="circle" v-if="isActive(tag)"></span>
-                {{ tag.title }}
-                <el-icon v-if="!isAffix(tag)" @click.prevent.stop="close(index)">
-                    <Close />
-                </el-icon>
-            </router-link>
-        </ScrollPane>
-    </div>
+  <div class="tags-view-container">
+    <ScrollPane>
+      <router-link
+        class="tags-view-item"
+        :class="{ active: isActive(tag) }"
+        v-for="(tag, index) in visitedViews"
+        :to="{ path: tag.path, query: tag.query, fullPath: tag.fullPath }"
+        :key="tag.path"
+      >
+        <span class="circle" v-if="isActive(tag)"></span>
+        {{ tag.title }}
+        <el-icon v-if="!isAffix(tag)" @click.prevent.stop="close(tag, index)">
+          <Close />
+        </el-icon>
+      </router-link>
+    </ScrollPane>
+  </div>
 </template>
 
-<script setup >
+<script setup>
 import ScrollPane from './ScrollPane.vue';
 import { useTagsViewsStore } from '@/stores/modules/tagsViews';
-
 
 const route = useRoute();
 const router = useRouter();
 const tagsView = useTagsViewsStore();
 const visitedViews = computed(() => tagsView.visitedViews);
-const isActive = computed(() => {
-    return (v) => {
-        return v.path === route.path;
-    }
-})
 const isAffix = computed(() => {
-    return (v) => {
-        return v?.meta?.affix;
-    }
-})
+  return (v) => {
+    return v?.meta?.affix;
+  };
+});
 
-
+function isActive(tag) {
+  return tag.path === route.path;
+}
 /**
  * 添加标签
  */
 function addTags() {
-    const { name } = route;
-    if (name) {
-        tagsView.addVisitedView(route);
-    }
+  const { name } = route;
+  if (name) {
+    tagsView.addVisitedView(route);
+  }
 }
 
 /**
  * 关闭
  */
-function close(index) {
-    tagsView.removeView(index);
-    GoToLastView();
+function close(tag, index) {
+  //close active tag
+  tagsView.removeView(index);
+  if (isActive(tag)) {
+    GoToLastView(visitedViews, tag);
+  }
 }
 
 /**
  * 跳转到最后一个标签
  */
-function GoToLastView() {
-    const { path, query } = tagsView.visitedViews[tagsView.visitedViews.length - 1];
-    router.push({ path, query });
+function GoToLastView(visitedViews, view) {
+  const latestView = visitedViews.value.slice(-1)[0];
+  if (latestView) {
+    router.push(latestView.fullPath);
+  }
 }
 
 watch(route, () => {
-    addTags()
-})
+  addTags();
+});
 onMounted(() => {
-    addTags()
-})
+  addTags();
+});
 </script>
 
 <style scoped lang="scss">
 .tags-view-container {
-    height: 34px;
-    width: 100%;
+  height: 34px;
+  width: 100%;
 
-    .tags-view-item {
-        display: inline-block;
-        height: 25px;
-        line-height: 25px;
-        border: 1px solid #d8dce5;
-        margin-left: 10px;
-        padding: 0 8px;
-        font-size: 12px;
-        color: #333;
-        border-radius: 5px;
-        white-space: nowrap;
+  .tags-view-item {
+    display: inline-block;
+    height: 25px;
+    line-height: 25px;
+    border: 1px solid #d8dce5;
+    margin-left: 10px;
+    padding: 0 8px;
+    font-size: 12px;
+    color: #333;
+    border-radius: 5px;
+    white-space: nowrap;
 
-        /* 圆 */
-        .circle {
-            display: inline-block;
-            height: 10px;
-            width: 10px;
-            border-radius: 50%;
-            background-color: #fff;
-        }
-
-        /* 关闭图标 */
-        i {
-            top: 2px;
-
-            &:hover {
-                transform: scale(1.5);
-            }
-        }
-
-        /* 激活状态 */
-        &.active {
-            background-color: rgb(64, 158, 255);
-            color: #fff;
-        }
+    /* 圆 */
+    .circle {
+      display: inline-block;
+      height: 10px;
+      width: 10px;
+      border-radius: 50%;
+      background-color: #fff;
     }
 
+    /* 关闭图标 */
+    i {
+      top: 2px;
 
+      &:hover {
+        transform: scale(1.5);
+      }
+    }
+
+    /* 激活状态 */
+    &.active {
+      background-color: rgb(64, 158, 255);
+      color: #fff;
+    }
+  }
 }
 </style>
