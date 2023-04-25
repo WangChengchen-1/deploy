0288f7706dc58ac9d7fc079c73d1ae29e7d2373c
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-25 13:41:14 +0800

    feat(学习 transitiongroup): 学习 transitionGroup
    学习 transitionGroup

--- src/views/web/vue/builtInComponent/transitionGroup/index.vue
+++ src/views/web/vue/builtInComponent/transitionGroup/index.vue
@@ -5,9 +5,36 @@
  * @name: TransitionGroup 内置组件
 -->
 <template>
-  <div class=""></div>
+  <div class="page-container">
+    <el-button type="primary" @click="add">添加元素</el-button>
+    <el-button @click="remove">移除元素</el-button>
+    <TransitionGroup name="list" tag="ul">
+      <li v-for="item in items" :key="item">
+        {{ item }}
+      </li>
+    </TransitionGroup>
+  </div>
 </template>
 
-<script setup ></script>
+<script setup>
+const items = ref([1, 2, 3, 4, 5, 6]);
+function add() {
+  let a = items.value[items.value.length - 1];
+  items.value.push(++a);
+}
+function remove() {
+  items.value.length -= 1;
+}
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
